212abbdfb28de94dea97091d461e5c24fe737fc3
reliable99 <abdulmuhminsodiq@gmail.com>
2024-06-01 14:04:09 +0100

    update

--- src/container/blog/Blog.jsx
+++ src/container/blog/Blog.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
-import './Blog.css'
-const Blog = () => {
-  return (
-    <div>Blog</div>
-  )
-}
+import React from 'react';
+import Article from '../../components/article/Article';
+import { blog01, blog02, blog06, blog04, blog07 } from './import';
+import './Blog.css';
 
-export default Blog
+const Blog = () => (
+  <div className="gpt3__blog section__padding" id="blog">
+    <div className="gpt3__blog-heading">
+      <h1 className="gradient__text">A lot is happening, <br /> We are blogging about it.</h1>
+    </div>
+    <div className="gpt3__blog-container">
+      <div className="gpt3__blog-container_groupA">
+        <Article imgUrl={blog01} date="Sep 26, 2021" text="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
+      </div>
+      <div className="gpt3__blog-container_groupB">
+        <Article imgUrl={blog02} date="Sep 26, 2021" text="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
+        <Article imgUrl={blog06} date="Sep 26, 2021" text="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
+        <Article imgUrl={blog04} date="Sep 26, 2021" text="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
+        <Article imgUrl={blog07} date="Sep 26, 2021" text="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
+      </div>
+    </div>
+  </div>
+);
+
+export default Blog;
---
+++ src/container/blog/import.js
@@ -0,0 +1,13 @@
+import blog01 from '../../assets/blog01.png';
+import blog02 from '../../assets/blog02.png';
+import blog06 from '../../assets/blog06.png'
+import blog04 from '../../assets/blog04.png';
+import blog07 from '../../assets/blog07.png';
+
+export {
+  blog01,
+  blog02,
+  blog06,
+  blog04,
+  blog07,
+};
